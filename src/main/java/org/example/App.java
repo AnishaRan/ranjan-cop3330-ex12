ae1e6f5770e9646c5ee9a633ef169e413de74709
Anisha Desktop <anisharanjan55@gmail.com>
2021-06-06 02:44:40 -0400

    Solution w/ Comment

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,12 +1,28 @@
 package org.example;
+import java.util.Scanner;
 
-/**
- * Hello world!
- *
+/*
+ *  UCF COP3330 Summer 2021 Assignment 12 Solution
+ *  Copyright 2021 Anisha Ranjan
  */
 public class App 
 {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the principal: ");
+        int principle = scan.nextInt();
+        System.out.print("Enter the rate of interest: ");
+        double wholePer = scan.nextDouble();
+        double per = wholePer / 100.0;
+        System.out.print("Enter the number of years: ");
+        int year = scan.nextInt();
+
+        double simpleInt = principle * (1 + (per * year));
+
+        System.out.printf("After %d years at %.1f%%, the investment will be worth $%.0f.", year, wholePer, simpleInt);
+
+
     }
 }
